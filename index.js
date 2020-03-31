3504f52e60d8a2098ceb1756623376021d53dff4
cool-library-2371 <est.ariel@gmail.com>
2020-03-31 18:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] ;//define your array here
 
 // Add your functions and code here
+console.log(kittens.pop());
+
+
+
+console.log(kittens.push());
+
+console.log(kittens.shift());
+
+console.log(kittens.unshift());
